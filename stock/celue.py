4e74e7dfa041b18e39f27138293f020031e15183
tommy.shen <11522857@qq.com>
2018-05-11 23:25:25 +0800

    add ma20

--- stock/celue.py
+++ stock/celue.py
@@ -2,13 +2,27 @@
 import tushare as ts
 import pandas as pd
 import datetime
+import sqlalchemy
+import pymysql
+pymysql.install_as_MySQLdb()
 
-data_today = ts.get_today_all()
-codes = data_today.code
-history = pd.read_sql_query('select data,ma20')
+
+#data_today = ts.get_today_all()
+#codes = data_today.code
+engine = sqlalchemy.create_engine('mysql://root:melody@127.0.0.1/stocks?charset=utf8')
+today = pd.read_sql_table('today',engine)
+hist = pd.read_sql_query('select date,close,code from stock_k', engine, index_col='date', parse_dates = True)
 
 '''如果今日收盘价高于20日均价则返回股票代码'''
 def close_high_ma20():
-    res = []
-    for code in codes:
-        if data_today[data_today.code == code].trade >=
+    df1 = hist.pivot(columns = 'code')
+    df2 = df1.rolling(window=20).mean().iloc[-1,].reset_index().iloc[:,1:].dropna()
+    df2.columns = ['code','ma20']
+    df3 = pd.merge(df2,today[['code','trade']])
+    res = list(df3[df3.trade >= df3.ma20].code)
+    return res
+
+res = close_high_ma20()
+print(len(res))
+
+
